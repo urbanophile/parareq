79e66f9fff2d075d52c202c3e1dcbe192e08f810
Matt Gibson <contact.matt.gibson@gmail.com>
2024-04-09 16:25:24 +1000

    fix cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,6 +15,10 @@ def test_cli_entrypoint_dry_run():
     with open("tests/data/config_example.jsonl", "w") as f:
         f.write('{"input": "Hello, world!"}\n')
         f.write('{"input": ["Hello", "world!"]}\n')
+    # create env var
+    os.environ["OPENAI_API_KEY"] = "xyz123"
+
+    # test condition
 
     exit_status = os.system(
         "parareq --dry_run --save_filepath output_example.jsonl --requests_filepath tests/data/config_example.jsonl"
